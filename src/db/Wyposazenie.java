31a1ece6fb8aaee70c6a1e8e97e881ecf67f60e2
przytula94 <przytula94@gmail.com>
2018-05-05 23:25:59 +0200

    Dodanie konstruktorów i getterów - wypozyczenie

--- src/db/Wyposazenie.java
+++ src/db/Wyposazenie.java
@@ -6,4 +6,28 @@ public class Wyposazenie {
     private String Klimatyzacja; //Wartosci: brak, manualna, automatyczna
     private String RodzajTapicerki;
     private String RodzajReflektorow;// wartosci: zwykle, led, xenon, bixenon
+
+    public Wyposazenie(int idWyposazenia, String klimatyzacja, String rodzajTapicerki, String rodzajReflektorow ) {
+
+        this.IdWyposazenia = idWyposazenia;
+        this.Klimatyzacja =klimatyzacja;
+        this.RodzajTapicerki =rodzajTapicerki;
+        this.RodzajReflektorow = rodzajReflektorow;
+    }
+    
+    public int getIdWyposazenia() {
+        return IdWyposazenia;
+    }
+
+    public String getKlimatyzacja() {
+        return Klimatyzacja;
+    }
+
+    public String getRodzajTapicerki() {
+        return RodzajTapicerki;
+    }
+
+    public String getRodzajReflektorow() {
+        return RodzajReflektorow;
+    }
 }
