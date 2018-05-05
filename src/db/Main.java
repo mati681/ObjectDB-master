7b42105f821594ac4080c20e44e6bcfad0786f8a
Mati <mateuszprekiel@gmail.com>
2018-05-05 20:55:03 +0200

    Initial commit

---
+++ src/db/Main.java
@@ -0,0 +1,19 @@
+package db;
+
+import javax.persistence.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("$objectdb/db/komis.odb");
+        EntityManager em = emf.createEntityManager();
+
+        // Close the database connection:
+        em.close();
+        emf.close();
+    }
+}
+
