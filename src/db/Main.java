68061db0a69057173f2f9e7239bc8c58fe290775
przytula94 <przytula94@gmail.com>
2018-05-06 23:38:02 +0200

    Dodano nową klasę generującą dane

--- src/db/Main.java
+++ src/db/Main.java
@@ -1,16 +1,16 @@
 package db;
 
 import javax.persistence.*;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Open a database connection
         // (create a new database if it doesn't exist yet):
-        EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("$objectdb/db/komis.odb");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("$objectdb/db/komis.odb");
         EntityManager em = emf.createEntityManager();
-
+        em.getTransaction().begin();
         // Close the database connection:
         em.close();
         emf.close();
